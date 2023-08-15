33b3eb5bc0d5d7a4c140b618de9c5eaedf77a2b0
Muhammad khaled <36699677+muhammedkhaled@users.noreply.github.com>
2023-08-15 12:45:24 +0300

    Update MainActivity.kt
    remove unneeded default code

--- app/src/main/java/com/mukhaled/englishpremierleagueapp/core/presentation/MainActivity.kt
+++ app/src/main/java/com/mukhaled/englishpremierleagueapp/core/presentation/MainActivity.kt
@@ -43,19 +43,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EnglishPremierLeagueAppTheme {
-        Greeting("Android")
-    }
-}
